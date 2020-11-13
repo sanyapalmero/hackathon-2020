beff4ff6ad2904fdfd2631611aadcba471e3a4b8
Alexandr Vagner <sanyapalmero@gmail.com>
2020-11-14 00:06:54 +0500

    Removed HomeView from users app

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='home'),
+
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,1 @@
 from django.views import generic
-
-
-class HomeView(generic.TemplateView):
-    template_name = 'users/home.html'
