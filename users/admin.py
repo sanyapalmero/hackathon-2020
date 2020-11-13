765ad3563391c3e0dfd47e1f8564366bf3e37788
Alexandr Vagner <sanyapalmero@gmail.com>
2020-11-14 00:42:31 +0500

    Display users table in django admin and unregister Group model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,19 @@
-from django.contrib import admin
+from django.contrib import admin, auth
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User
+
+admin.site.unregister(auth.models.Group)
+
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'username', 'role', 'last_login', 'created_at')
+    search_fields = ('email', 'username', 'role')
+    readonly_fields = ('last_login',)
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+
+admin.site.register(User, CustomUserAdmin)
--- users/models.py
+++ users/models.py
@@ -56,3 +56,7 @@ class User(AbstractBaseUser):
 
     def has_module_perms(self, app_label):
         return self.is_admin
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
