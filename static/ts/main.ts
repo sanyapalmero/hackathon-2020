a68ea87f74b72843de37e93e41a12b42e8435d1d
Oleg Kuznetsov <mr.defix@gmail.com>
2020-11-15 18:45:55 +0500

    Complete with asset detail

--- static/ts/main.ts
+++ static/ts/main.ts
@@ -3,6 +3,8 @@ import "../scss/main.scss";
 import $ from 'jquery';
 import 'bootstrap';
 import bsCustomFileInput from 'bs-custom-file-input'
+import Swiper, {Navigation, Pagination} from "swiper";
+
 
 // Local
 import ModalValue from './components/ModalValue';
@@ -13,21 +15,30 @@ import ResetInputElement from "./components/ResetInputElement";
 
 // Set value for modal window
 $('.ModalValue').each((_index, htmlElement) => {
-  new ModalValue($(htmlElement));
+    new ModalValue($(htmlElement));
 });
 
 $(function () {
-  bsCustomFileInput.init()
+    bsCustomFileInput.init()
 })
 
 $('.YandexMap').each((index, htmlElement) => {
-  new YandexMap(htmlElement);
+    new YandexMap(htmlElement);
 });
 
 $('.DemoFill').each((index, htmlElement) => {
-  new DemoFill(htmlElement);
+    new DemoFill(htmlElement);
 });
 
-$(".ResetInput").each((index, htmlElement)=>{
-  new ResetInputElement(htmlElement);
+$(".ResetInput").each((index, htmlElement) => {
+    new ResetInputElement(htmlElement);
+});
+
+Swiper.use([Navigation, Pagination]);
+
+new Swiper('.swiper-container', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
 });
